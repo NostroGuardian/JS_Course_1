163354ec990c3e33945e1d04db23fd5e6a7deed5
unknown <shmelivan5@yandex.ru>
2023-06-27 17:09:19 +0300

    ternar + concat + bool

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const res = prompt('Сколько будет 7 + 15 или 7 - 15?');
+const balance = 900;
+const bonusBalance = 100;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+const canBuy =
+  (balance > 1000 || bonusBalance >= 100) && !isBanned && isSelling && !isExist;
+
+console.log(`Могу купить игру: ${canBuy ? 'Yes' : 'No'}`);
