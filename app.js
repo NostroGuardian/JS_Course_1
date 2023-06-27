36089f1638ea726745833f0202b43c0e759dcae1
unknown <shmelivan5@yandex.ru>
2023-06-27 15:26:50 +0300

    ternar

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('test');
+const audiA3 = 100000;
+const bycicle = 10000;
+const budget = 10000;
+
+console.log(
+  `Я хочу купить ${
+    budget >= audiA3 ? 'Audi A3' : budget >= bycicle ? 'bycicle' : 'Foot'
+  }`
+);
