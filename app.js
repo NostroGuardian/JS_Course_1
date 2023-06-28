c36b1de3ee495802a4ce76b3218763530359dadf
unknown <shmelivan5@yandex.ru>
2023-06-28 14:04:41 +0300

    massive

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-/*
-Пользователь:
+const role1 = 'admin';
+const role2 = 'moderator';
+const role3 = 'user';
 
-    Возраст
-    Наличие работы
-    Деньги
+const roles = ['admin', 'moderator', 'user'];
 
-Нужно проверить может ли он купить новый MacBook за 2000? Он может брать не только свои деньги, но и взять кредит. Ему дадут 500, только если ему больше 24-х лет и он имеет работу, 100 если ему просто больше 24-х лет и 0 в ином случае. Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false.
-*/
+console.log(roles[0]);
+console.log(roles.length);
+console.log(roles[roles.length - 1]);
 
-function creditLimit(age, work) {
-  if (age > 24 && work === 'Yes') {
-    return 500;
-  } else if (age > 24 && work === 'No') {
-    return 100;
-  } else {
-    return Number(0);
-  }
+console.log(roles.at(-1));
+
+function square(el) {
+  return el + el;
 }
 
-function canBuyMacBook(age, work, money) {
-  const creditAdd = creditLimit(age, work);
-  const userMoney = money + creditAdd;
-  return userMoney >= 2000 ? true : false;
-}
-
-console.log(canBuyMacBook(24, 'No', 2000));
+console.log(square([1, 2, 3]));
