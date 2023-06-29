424dcedfb2398e04c8915f63b5e78de94db9a0a1
unknown <shmelivan5@yandex.ru>
2023-06-29 20:37:09 +0300

    destructurization

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ console.log(aname, age, adr);
 
 const userData2 = ['Ivan', 23, 'Moscow'];
 
-const [aname2, _, adr2] = userData;
+const [aname2, _, adr2] = userData2;
 console.log(aname2, adr2);
+
+const userData3 = ['Ivan', 23, 'Moscow'];
+
+const [aname3, ...others] = userData3;
+console.log(aname3, others);
