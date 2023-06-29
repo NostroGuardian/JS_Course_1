8b3660954e0632d767918a7db9958e92462c507f
unknown <shmelivan5@yandex.ru>
2023-06-29 20:29:43 +0300

    destructurization

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-const users = ['Anya', 'Vika', 'Katya'];
-console.log(users);
+const userData = ['Ivan', 23, 'Moscow'];
 
-users[2] = 'Kris';
-console.log(users);
-
-const arrLenght = users.push('Ivan');
-console.log(users, arrLenght);
-
-users.unshift('Liza');
-console.log(users);
-
-const el = users.pop();
-console.log(users);
-console.log(el);
-
-const el2 = users.shift();
-console.log(users);
-console.log(el2);
+const [aname, age, adr] = userData;
+console.log(aname, age, adr);
