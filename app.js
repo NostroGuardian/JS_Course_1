7f499134fe81079db6f6a750b2b0a31ad49863d4
unknown <shmelivan5@yandex.ru>
2023-06-30 16:04:12 +0300

    for

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const userData = ['Ivan', 23, 'Moscow'];
+const tasks = ['task1', 'task2', 'task3'];
 
-const [aname, age, adr] = userData;
-console.log(aname, age, adr);
+for (let i = 0; i < tasks.length; i++) {
+  if (tasks[i] === 'task2') {
+    continue;
+  }
+  console.log(tasks[i]);
+}
 
-const userData2 = ['Ivan', 23, 'Moscow'];
+console.log('--------');
 
-const [aname2, _, adr2] = userData2;
-console.log(aname2, adr2);
-
-const userData3 = ['Ivan', 23, 'Moscow'];
-
-const [aname3, ...others] = userData3;
-console.log(aname3, others);
+for (let i = 0; i < tasks.length; i++) {
+  if (tasks[i] === 'task2') {
+    break;
+  }
+  console.log(tasks[i]);
+}
