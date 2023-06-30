82159e2ff756804a39339525eed7828c38171022
unknown <shmelivan5@yandex.ru>
2023-06-30 18:07:49 +0300

    callback

--- app.js
+++ app.js
@@ -1,65 +1,11 @@
-/* 
-
-Есть выгрузка операций пользователя
-
-const operations = [1000, -700, 300, -500, 10000];
-
-а так же начальный баланс в 100$. Необходимо сделать функции расчёта:
-
-    Итогового баланса
-    Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
-    Расчёта среднего расхода и среднего дохода
-
-*/
-
-const operations = [1000, -700, 300, -500, 10000]; //10100
-const startBalance = 100;
-
-function resultBalance(startBalance, operations) {
-  let res = 0;
-  for (let el of operations) {
-    res += el;
-  }
-  return res + startBalance;
+function add(a, b) {
+  return a + b;
 }
 
-function negativeBalance(startBalance, operations) {
-  let res = 0 + startBalance;
-  const isBalanceOk = true;
-  for (el of operations) {
-    res += el;
-    if (res < 0) {
-      isBalanceOk = false;
-      break;
-    }
-  }
-  return isBalanceOk;
+function subtract(a, b) {
+  return a - b;
 }
 
-function computeMiddleExpenditure(operations) {
-  let res = [];
-  for (el of operations) {
-    if (el < 0) {
-      res.push(el);
-    }
-  }
-  let sum = 0;
-  for (el of res) {
-    sum += el;
-  }
-  return sum / res.length;
-}
-
-function computeMiddleIncome(operations) {
-  let res = [];
-  for (el of operations) {
-    if (el > 0) {
-      res.push(el);
-    }
-  }
-  let sum = 0;
-  for (el of res) {
-    sum += el;
-  }
-  return sum / res.length;
+function calculate(a, b, fn) {
+  return fn(a, b);
 }
